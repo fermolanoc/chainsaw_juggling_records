6af548e79f7ff5787913e2b964c2337ef9311da0
Fernando Molano <fm87usa@gmail.com>
2021-09-21 23:23:18 -0500

    Get data from user input and save it into tuple

--- chainsaw_juggling_records.py
+++ chainsaw_juggling_records.py
@@ -26,3 +26,24 @@ class Player(RecordModel):
 # Connect to DB and create tables that map to Models Player and Country
 db.connect()
 db.create_tables([Country, Player])
+
+# Empty list to store each player data
+data = []
+
+# Ask user for Player data to insert into DB tables
+enter_record = input("Press 'Y' to add a record or any other key to quit ")
+
+while enter_record.upper() == 'Y':
+    player_name = input('Enter player name: ')
+    player_country = input('Which country does this player represents? ')
+    number_of_catches = int(input('Now enter number of catches record: '))
+
+    player_record = (player_country, player_name, number_of_catches)
+    data.append(player_record)
+
+    enter_record = input(
+        "Press 'Y' to add another record or any other key to quit ")
+
+# print(data)
+data_tuple = tuple(data)
+# print(data_tuple)
