bd7d2192559bc538962f83608076e08a76942b2b
Fernando Molano <fm87usa@gmail.com>
2021-09-21 22:53:17 -0500

    Update base model name, add 2 children Model with corresponding object fields, create tables based on model classes

--- chainsaw_juggling_records.py
+++ chainsaw_juggling_records.py
@@ -5,6 +5,24 @@ db = SqliteDatabase('chainsaw_juggling_records.sqlite')
 
 
 # Create Base Model
-class Record(Model):
+class RecordModel(Model):
     class Meta:
         database = db
+
+
+# Model Country to store only country names
+class Country(RecordModel):
+    name = CharField()
+
+
+# Model Player to store chainsaw juggling players who are on the record list
+class Player(RecordModel):
+    name = CharField()
+    number_of_catches = IntegerField()
+    # foreign key related to Country Model/Table
+    country = ForeignKeyField(Country, backref='players')
+
+
+# Connect to DB and create tables that map to Models Player and Country
+db.connect()
+db.create_tables([Country, Player])
